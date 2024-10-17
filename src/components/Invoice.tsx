7d5059cafe3152df95a3a497aaac33520a4d141c
jose_joga <jose_joga@bhd.com.do>
2024-10-17 08:09:27 -0400

    invoice numbers

--- src/components/Invoice.tsx
+++ src/components/Invoice.tsx
@@ -75,6 +75,7 @@ const Invoice: React.FC<InvoiceProps> = ({ submission, onClose }) => {
                 <div><strong>Tutorías Universitarias</strong></div>
                 <div>soporte.tutoriasuniversitarias@gmail.com</div>
                 <div>+34 608 83 72 72</div>
+                <div>+1 849 270 1295</div>
               </div>
             </div>
             
@@ -180,6 +181,7 @@ const Invoice: React.FC<InvoiceProps> = ({ submission, onClose }) => {
                 soporte.tutoriasuniversitarias@gmail.com
               </div>
               <div className="text-gray-600">+34 608 83 72 72</div>
+              <div className="text-gray-600">+1 849 270 1295</div>
             </div>
           </div>
 
